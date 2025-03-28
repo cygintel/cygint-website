d6d84f700b19e696dba16e4019f9da2ede7d4a22
Nilesh Chaudhari <nilesh@cygint.co>
2025-03-28 16:09:22 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://cygint.github.io",
   integrations: [
-    react(), 
+    react(),
     tailwind({
       // Use our existing Tailwind config
-      config: { path: './tailwind.config.ts' },
-    })
+      config: { path: "./tailwind.config.ts" },
+    }),
   ],
-  output: 'server', // Enable SSR mode for API routes
+  output: "server", // Enable SSR mode for API routes
 });
