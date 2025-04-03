fe55b55c929ca1a9beaf96406df4f9d892e9983e
Nilesh Chaudhari <nilesh@nilmacbookpro.local>
2025-04-03 13:49:59 +0530

    Refactored code to run a simple static website.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,9 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static', // Use 'static' for GitHub Pages
-  site: 'https://cygint.co', // Your actual production site URL
-  integrations: [
-    tailwind(),
-    react()
-  ],
-  server: {
-    host: '0.0.0.0',
-    port: 4321
-  }
+  integrations: [react(), tailwind()]
 });
