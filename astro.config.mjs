105886d6fcae7a2700578259f0c7b34d297f70df
Nilesh Chaudhari <mail@nilesh.org>
2025-04-03 14:27:49 +0530

    Updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  site: "http://cygint.co",
+  base: "/",
+  output: "static",
+  integrations: [react(), tailwind()],
 });
